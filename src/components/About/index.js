d1533a7c7c98f8ae2c55ba82024c6c03f574b916
Cameron O'Brien <obriecam@oregonstate.edu>
2023-02-24 19:16:43 -0800

    Updated About me section and resized margins for differing platform viewing

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,29 +8,27 @@ const About = () => {
                 <h1>
                     It's nice to meet you!
                 </h1>
-                <p>
-                    I recently graduated from Oregon State University with a Bachelor's Degree in 
-                    Computer Science with a minor in Business Administration. I am actively seeking
-                    professional opportunities in software engineering. 
+                    <p>
+                        I am an Oregon State University graduate with a Bachelor's Degree in Computer Science and a Minor in Business Administration.
+                        I am actively pursuing professional opportunities in cloud software engineering and architecture.
 
-                </p>
-                <p>
-                    My time at Oregon State provided me with ample experience in many development fields.
-                    I currently find myself most interested and experienced with full stack web development
-                    but am open to new opportunites. 
+                    </p>
+                    <p>
+                        My time at Oregon State provided me with ample experience in many development fields.
+                        I currently find myself most interested and experienced with full stack web development
+                        but am open to new opportunites. 
 
-                </p>
-                <p>
-                    My current plan going forward is to familiarize myself
-                    with the professional software engineering environment to gear myself to become a project
-                    manager or scrum master in the near future. I believe my development knowledge combined
-                    with my communication skills place me in a unique position to lead.
-                </p>
-                <p>
-                    As a new graduate I am extremely excited to get to work and make professional connections!
-                    Please feel free to get in contact with me through my contact page or on <a href="https://www.linkedin.com/in/cameron-obrien4/" target="_blank" rel="noreferrer noopener">LinkedIn.</a>
+                    </p>
+                    <p>
+                        In recent months I have switched my focus to developing knowledge in cloud based architecture
+                        and development. I have earned the AWS Solutions Architect Associate and Cloud Practitioner
+                        Certifications and plan on continuing my pursuit for more. 
+                    </p>
+                    <p>
+                        As a new graduate I am extremely excited to get to work and make professional connections!
+                        Please feel free to get in contact with me through my contact page or on <a href="https://www.linkedin.com/in/cameron-obrien4/" target="_blank" rel="noreferrer noopener">LinkedIn.</a>
 
-                </p>
+                    </p>
 
             </div>
         </div>
